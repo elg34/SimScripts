d67409de0d7cf439266ec7ce6d6e5732fe6b8e6d
Luise Gootjes-Dreesbach <elgootjesdreesbach@gmail.com>
2018-06-19 13:25:28 +0100

    mixed model 2

--- signal_detection_spheres/illustration3.R
+++ signal_detection_spheres/illustration3.R
@@ -28,7 +28,8 @@ verghese_mod2<-function(sig,n_targ,n_dist,sim, t_type = FALSE){
     nosign_gl<-max(rnorm(n_dist+n_targ,mu1,sd1))
     sign_gl<-if (n_dist==0){max(rnorm(n_targ,mu2,sd2))}else if (n_targ==0) {max(rnorm(n_dist,mu1,sd1))}else if (t_type==TRUE){max(c(rnorm(n_dist,mu2,sd2),rnorm(n_targ,sig^2,sd2)))}else{max(c(rnorm(n_dist,mu1,sd1),rnorm(n_targ,mu2,sd2)))}
     nosign_rel<-max(rnorm(n_dist+n_targ,mu1,sd1))
-    sign_rel<-if (n_dist==0){max(rnorm(n_dist+n_targ,mu1,sd1))}else{max(c(rnorm(7,mu1,sd1),rnorm(1,mu2,sd2)))}
+    sign_rel<-if (n_dist==0){max(rnorm(n_dist+n_targ,mu1,sd1))}else if (t_type==TRUE){max(c(rnorm(7,mu2,sd2),rnorm(1,mu2^2,sd2)))}else{max(c(rnorm(7,mu1,sd1),rnorm(1,mu2,sd2)))}
+    
     
     nosign[i]<-max(nosign_gl,nosign_rel)
     sign[i]<-max(sign_gl,sign_rel)
@@ -50,6 +51,6 @@ df<-data.frame(
 )
 
 
-p <- ggplot(df, aes(x=dv, fill=s)) + geom_density(alpha=.3) + facet_wrap(~ c) + ggtitle(paste("Underlying Signal: ",sig)) + theme(text = element_text(size=20),plot.title = element_text(hjust = 0.5, face="bold"))
+p <- ggplot(df, aes(x=dv, fill=s)) + geom_density(alpha=.3) + facet_wrap(~ c) + ggtitle(paste("Underlying Signal (T1/D7): ",sig)) + theme(text = element_text(size=20),plot.title = element_text(hjust = 0.5, face="bold"))
 vline.data <- data.frame(z = c((mean(mod1$sign)+mean(mod1$nosign))/2,(mean(mod2$sign)+mean(mod2$nosign))/2), c = c('Model 1', 'Model 2'))
 p + geom_vline(aes(xintercept = z), vline.data)
