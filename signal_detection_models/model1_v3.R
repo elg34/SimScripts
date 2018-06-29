4bb85eae9bcd598f16571cbf181df38a9594f02f
Luise Gootjes-Dreesbach <elgootjesdreesbach@gmail.com>
2018-06-29 08:36:24 +0100

    WIP

--- signal_detection_models/model1_v3.R
+++ signal_detection_models/model1_v3.R
@@ -4,24 +4,24 @@ library(psyphy)
 library(ggplot2)
 library(zoo)
 
-verghese_mod<-function(sig,n_targ,n_dist,sim, t_type = FALSE){
-  mu1<-0
-  mu2<-sig # signal size (should be dprime of single target ident.)
-  sd<-1
+verghese_mod<-function(sig,n_targ,n_dist,sim, t_type = FALSE,opt=FALSE){
+  numit<-n_dist+n_targ
   sign<-vector(length = round(sim/2))
   nosign<-vector(length = round(sim/2))
   for (i in 1:round(sim/2)){ 
-    # 2 kinds of trials
-    nosign[i]<-max(rnorm(n_dist+n_targ,mu1,sd)) # get numit samples from distractor distribution, take maximum
-    sign[i]<-if (n_dist==0){max(rnorm(n_targ,mu2,sd))}else if (n_targ==0) {max(rnorm(n_dist,mu1,sd))}else if (t_type==TRUE){max(c(rnorm(n_dist,mu2,sd),rnorm(n_targ,sig^2,sd)))}else{max(c(rnorm(n_dist,mu1,sd),rnorm(n_targ,mu2,sd)))} # get numit-1 samples from distractor distribution and 1 from target distribution, take maximum
+    nosign[i]<-max(rnorm(n_dist+n_targ,0,1)) # get numit samples from distractor distribution, take maximum
+    sign[i]<-if (n_dist==0){max(rnorm(n_targ,sig,1))}else if (n_targ==0) {max(rnorm(n_dist,0,1))}else if (t_type==TRUE){max(c(rnorm(n_dist,sig,1),rnorm(n_targ,sig^2,1)))}else{max(c(rnorm(n_dist,0,1),rnorm(n_targ,sig,1)))}
   }
-  
   cs<-sort(c(sign,nosign))
   fp<-mapply(function(c,sim) length(nosign[nosign>c])/(sim/2),cs,sim)
   hit<-mapply(function(c,sim) length(sign[sign>c])/(sim/2),cs,sim)
   AUC <- abs(sum(diff(fp)*rollmean(hit,2)))
   dp <- qnorm(AUC)*sqrt(2)
-  list('AUC'=AUC,'dp'=dp)
+  if (opt==FALSE){
+    dp
+  }else{
+    abs(dp-opt)
+  }
 }
 
 targ<-c(1,7,8,1)
@@ -29,19 +29,15 @@ dist<-c(7,1,0,7)
 t_type<-c(FALSE,FALSE,FALSE,TRUE)
 label<-mapply(function(x,y,z) paste("T:",x,"/","D:",y,'/T:B^2=',z,sep=''), targ, dist, t_type)
 
-sim<-10000
+sim<-1000
 xvals<-seq(0,6,0.5)
-AUC<-NULL
 dp<-NULL
 for (i in xvals){ # 'fp'=fp,'hit'=hit,'rej'=rej,'miss'=miss,'AUC'=AUC,'dp'=dp
-  dat<-mapply(verghese_mod, rep(i,length(targ)), targ, dist, sim,t_type)
-  AUC<-c(AUC,unlist(dat[1,]))
-  dp<-c(dp,unlist(dat[2,]))
+  dp<-c(dp,mapply(verghese_mod, rep(i,length(targ)), targ, dist, sim,t_type))
 }
 
 df<-data.frame(
   x=sort(rep(xvals,length(targ))),
-  AUC=AUC,
   dp=dp,
   group=c(t(mapply(function(x) rep(x,length(xvals)),label)))
 )
--- signal_detection_models/model2_v3.R
+++ signal_detection_models/model2_v3.R
@@ -4,20 +4,15 @@ library(psyphy)
 library(ggplot2)
 library(zoo)
 
-verghese_mod2<-function(sig,n_targ,n_dist,sim, t_type = FALSE){
-  mu1<-0
-  mu2<-sig # signal size (should be dprime of single target ident.)
-  sd1<-1    # SD of target detector responses to distractor (Default: 1)
-  sd2<-1    # SD of target detector responses to target (Default: 1)
-  
+verghese_mod2<-function(sig_gl,sig_rel,n_targ,n_dist,sim, t_type = FALSE,opt=FALSE){
+  numit<-n_dist+n_targ
   sign<-vector(length = round(sim/2))
   nosign<-vector(length = round(sim/2))
   for (i in 1:round(sim/2)){ 
-    # 2 kinds of trials
-    nosign_gl<-rnorm(n_dist+n_targ,mu1,sd1)
-    sign_gl<-if (n_dist==0){rnorm(n_targ,mu2,sd2)}else if (n_targ==0) {rnorm(n_dist,mu1,sd1)}else if (t_type==TRUE){c(rnorm(n_dist,mu2,sd2),rnorm(n_targ,sig^2,sd2))}else{c(rnorm(n_dist,mu1,sd1),rnorm(n_targ,mu2,sd2))}
-    nosign_rel<-max(rnorm(n_dist+n_targ,mu1,sd1))
-    sign_rel<-if (n_dist==0){rnorm(n_dist+n_targ,mu1,sd1)}else{c(rnorm(7,mu1,sd1),rnorm(1,mu2,sd2))}
+    nosign_gl<-rnorm(numit,0,1)
+    nosign_rel<-rnorm(numit,0,1)
+    sign_gl<-if (n_dist==0){rnorm(n_targ,sig_gl,1)}else if (n_targ==0) {rnorm(n_dist,0,1)}else if (t_type==TRUE){c(rnorm(n_dist,sig_gl,1),rnorm(n_targ,sig_gl^2,1))}else{c(rnorm(n_dist,0,1),rnorm(n_targ,sig_gl,1))}
+    sign_rel<-if (n_dist==0){rnorm(numit,0,1)}else{c(rnorm(numit-1,0,1),rnorm(1,sig_rel,1))}
     
     nosign[i]<-max(c(nosign_gl,nosign_rel))
     sign[i]<-max(c(sign_gl,sign_rel))
@@ -28,7 +23,11 @@ verghese_mod2<-function(sig,n_targ,n_dist,sim, t_type = FALSE){
   hit<-mapply(function(c,sim) length(sign[sign>c])/(sim/2),cs,sim)
   AUC <- abs(sum(diff(fp)*rollmean(hit,2)))
   dp <- qnorm(AUC)*sqrt(2)
-  list('AUC'=AUC,'dp'=dp)
+  if (opt==FALSE){
+    dp
+  }else{
+    abs(dp-opt)
+  }
 }
 
 targ<-c(1,7,8,1)
@@ -38,17 +37,13 @@ label<-mapply(function(x,y,z) paste("T:",x,"/","D:",y,'/T:B^2=',z,sep=''), targ,
 
 sim<-1000
 xvals<-seq(0,6,0.5)
-AUC<-NULL
 dp<-NULL
-for (i in xvals){ # 'fp'=fp,'hit'=hit,'rej'=rej,'miss'=miss,'AUC'=AUC,'dp'=dp
-  dat<-mapply(verghese_mod2, rep(i,length(targ)), targ, dist, sim,t_type)
-  AUC<-c(AUC,unlist(dat[1,]))
-  dp<-c(dp,unlist(dat[2,]))
+for (i in xvals){
+  dp<-c(dp,mapply(verghese_mod2, rep(i,length(targ)),rep(i,length(targ)), targ, dist, sim,t_type))
 }
 
 df<-data.frame(
   x=sort(rep(xvals,length(targ))),
-  AUC=AUC,
   dp=dp,
   group=c(t(mapply(function(x) rep(x,length(xvals)),label)))
 )
