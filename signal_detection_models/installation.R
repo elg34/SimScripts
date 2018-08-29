b9f4b01c9a1a6a76150906598b1acabe9b29c89e
Luise Gootjes-Dreesbach <elgootjesdreesbach@gmail.com>
2018-08-29 10:21:43 +0100

    updated structure

--- signal_detection_models/installation.R
+++
@@ -1,8 +0,0 @@
-library("devtools")
-library(roxygen2)
-
-setwd("C:/Users/Luise/Documents/GitHub/SimScripts/model")
-document()
-
-setwd("C:/Users/Luise/Documents/GitHub/SimScripts/")
-install("model")
